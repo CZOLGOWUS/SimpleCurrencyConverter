1990d401521681788d03e59d315242c9f27209bf
CZOLGOWUS <53970486+CZOLGOWUS@users.noreply.github.com>
2021-10-23 22:00:01 +0200

    add PRogram class

---
+++ SimpleCurrencyConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleCurrencyConverter
+{
+    class Program
+    {
+        Program instance = null;
+        private Program() { }
+
+        public Program GetInstance()
+        {
+            if( instance == null )
+                return new Program();
+            else
+                return GetInstance();
+        }
+
+        
+
+        static void Main( string[] args )
+        {
+            
+        }
+    }
+}
